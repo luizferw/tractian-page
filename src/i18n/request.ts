5bde6392ca5b32fac9314ac20ea7093894742044
luizferw <luizfernandowitt00gmail.com>
2026-01-20 06:49:06 -0300

    refactor: simplify locale resolution logic in request configuration

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,14 @@
-import { getRequestConfig } from 'next-intl/server';
-import { routing } from './routing';
-import { hasLocale } from 'next-intl';
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
+import { hasLocale } from "next-intl";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
-
-  if (!hasLocale(routing.locales, locale)) {
-    locale = routing.defaultLocale;
-  }
+export default getRequestConfig(async ({ locale }) => {
+  const resolvedLocale = hasLocale(routing.locales, locale)
+    ? locale
+    : routing.defaultLocale;
 
   return {
-    locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    locale: resolvedLocale,
+    messages: (await import(`../messages/${resolvedLocale}.json`)).default,
   };
 });
